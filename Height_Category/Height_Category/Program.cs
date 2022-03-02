35020fe56c8103890a6108c868f1ac6cd6b103e8
Jennifer Hajduk <jenniferhajduk@Jennifers-Air.localdomain>
2022-03-01 21:39:32 -0800

    comitting two new programs

---
+++ Height_Category/Height_Category/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Height_Category
+{
+    class HeightFunctions
+    {
+        public static string height_function(double h, out string hc)
+        {
+            if (h >= 66.0)
+            {
+                hc = "Tall";
+            }
+            else
+            {
+                hc = "Short";
+            }
+            return hc;
+        }
+    }
+    class DetermineHeight
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter a height in inches:");
+            double height = Convert.ToDouble(System.Console.ReadLine());
+            string hc = string.Empty;
+            HeightFunctions.height_function(height, out hc);
+            System.Console.WriteLine("The height you entered is categorized as: {0}", hc);
+        }
+    }
+}
---
+++ Largest_Number/Largest_Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Largest_Number
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            System.Console.WriteLine("Please enter positive numbers, each on a new line:");
+            double largest_number = double.NegativeInfinity; 
+            for (int i = 0; i < 3; i ++)
+            {
+                numbers[i] = Convert.ToInt32(System.Console.ReadLine());
+                if (numbers[i] > largest_number)
+                {
+                    largest_number = numbers[i];
+                }
+            }
+            System.Console.WriteLine("The largest number entered is: {0}", largest_number);
+        }
+    }
+}
